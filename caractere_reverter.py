1bcddd3bb5cfa6acc9ce582da79f15e616ff4e89
ilumishy <ilumiecch@gmail.com>
2023-03-08 15:44:36 -0300

    Invertendo strings com python

---
+++ caractere_reverter.py
@@ -0,0 +1,4 @@
+# O python possui diversas formas de se manipular uma string
+# essa por exemplo percorre uma string e reverte os caracteres
+string = str(input("Informe uma string qualquer: "))
+print(string[::-1])
